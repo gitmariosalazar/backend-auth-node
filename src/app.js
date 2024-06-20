f26badb73771a3dccb2102d5cc0982b3663d50f0
Mario Salazar <mariosalazar.ms.10@gmail.com>
2024-06-19 22:47:44 -0500

    Upload Swagger Document Updated 14

--- src/app.js
+++ src/app.js
@@ -55,7 +55,7 @@ app.use('/api/docs', swaggerUi.serve, (req, res, next) => {
     swaggerSpec.servers[0].url = `${domain}`
     swaggerUi.setup(swaggerSpec, {
         customCssUrl:
-            ".css/custom-css.css",
+            "/css/custom-css.css",
     })(req, res, next);
 });
 
